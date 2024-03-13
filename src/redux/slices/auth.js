52ea14ff785f52c077c2fd479613a477f96249f7
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-03-12 20:08:39 -0700

    auth functionality

---
+++ src/redux/slices/auth.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios';
+
+export const fetchAuth = createAsyncThunk('auth/fetchUserData', async (params) => {
+    const { data } = await axios.post('/auth/login', params);
+
+    return data;
+});
+
+const initialState = {
+    data: null,
+    status: 'loading',
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducer: {},
+    extraReducers: {
+        [fetchAuth.pending]: (state, action) => {
+            state.status = 'loading';
+            state.data = null;
+        },
+        [fetchAuth.fulfilled]: (state, action) => {
+            state.status = 'loaded';
+            state.data = action.payload;
+        },
+        [fetchAuth.rejected]: (state, action) => {
+            state.status = 'error';
+            state.data = [];
+        },
+    },
+});
+
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
+export const authReducer = authSlice.reducer;
